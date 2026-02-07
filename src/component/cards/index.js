e017c621961e7b3f8796f56bf485d86f7ef0a722
akashroy8210 <akashroy@iitg.ac.in>
2026-02-07 08:39:39 +0530

    correct file

--- src/component/cards/index.js
+++ src/component/cards/index.js
@@ -7,10 +7,6 @@ import Propose from './Propose'
 import Rose from './Rose'
 import Teddy from './Teddy'
 import Valentine from './Valentine'
-<<<<<<< HEAD
-=======
-import Kiss from './Rose'
->>>>>>> c3f1d3201ecc146831e34839a332fc4ca5d5cebc
 export {
   Choclate,
   Hug,
